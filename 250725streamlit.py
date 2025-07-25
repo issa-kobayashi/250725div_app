05a231a1769d359517fa93080fcfc2f0f8e39a0f
issa-kobayashi <issa580611@gmail.com>
2025-07-25 17:03:21 +0900

    250725-20

---
+++ 250725streamlit.py
@@ -0,0 +1,125 @@
+print('00-1基本import')
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+# imageﾊpip install pillowｶﾞ必要 pil
+import time
+#from datetime import datetime
+import datetime
+import matplotlib.pyplot as plt
+
+print('')
+print('01-1 titleの表示')
+st.title('250725streamlit')
+st.caption('副題の表示 町内の為のホームページです｡')
+
+
+#streamlit run e:\250725div_app\250725streamlit.py
+#上記をｺﾏﾝﾄﾞﾌﾟﾛﾝﾌﾟﾄに打ち込みﾛｰｶﾙﾎｽﾄを起動させる
+
+
+#縦に分轄
+col1,col2=st.columns(2)
+
+with col1:
+        st.subheader('みずき野紹介')
+        
+        st.text('みずき野は田んぼに囲まれたとても静かな街です｡')
+        st.write('プログレスバーの表示')
+        
+        
+        #画像の登録
+        #image=Image.open('iris_tree.png')
+        image=Image.open(r'E:\250725div_app\data250725re\IMG241103_115831.jpg')
+        
+        #streamlitﾆimageｦ渡す
+        st.image(image,width=400)
+        
+        image=Image.open(r'E:\250725div_app\data250725re\IMG241103_115854.jpg')
+        st.image(image,width=400)
+        
+        image=Image.open(r'E:\250725div_app\data250725re\IMG241103_115947.jpg')
+        st.image(image,width=400)
+        
+        #動画をupする
+        #video_file=open('744_640x360.mov','rb')
+#        video_file=open(r'E:\250725div_app\data250725re\DSC231123_(3).mp4')
+        
+        #streamlitﾆvideoｦ渡す
+        
+        #video_bytes=video_file.read()
+#        #st.video(video_bytes)
+        
+#        st.video(r'E:\250725div_app\data_250725\DSC231123.mp4')
+#       st.video(r'E:\250725div_app\data_250725\VID250511.mp4')
+        
+        
+#        #text boxｦ画面上に作る
+            
+#        with st.form(key='profile_form'):
+#            name=st.text_input('名前')
+#            address=st.text_input('住所')
+        
+#            print(name)
+        
+        
+#            #botannの作成 送信 ｷｬﾝｾﾙ
+#            #submit_btn=st.button('送信')
+#            submit_btn=st.form_submit_button('送信')
+#            #cancel_btn=st.button('キャンセル')
+#            cancel_btn=st.form_submit_button('キャンセル')
+#            print(f'submit_btn:{submit_btn}')
+#            print(f'cancel_btn:{cancel_btn}')
+            
+#            #select box
+#            #age_category=st.selectbox(
+#            age_category=st.radio(
+#                '年齢層',
+#                ('子供(18才未満)','大人(18才以上)'))
+            
+#            hobby=st.multiselect(
+#                '趣味',
+#                ('スポーツ','読書','プログラミング','アニメ ・ 映画','釣り','料理'))
+            
+#            mail_subscript=st.checkbox('メールマガジンを読む')
+            
+#            height=st.slider('身長',min_value=110,max_value=210)
+            
+#            start_date = st.date_input(
+#                 '開始日',
+#                 datetime.date(2025,5,1))
+            
+#            color=st.color_picker('カラー','#00f900')
+            
+            
+#        #上記はプロンプト画面で確認される｡
+#        #submit_btn:True
+#        #cancel_btn:False ﾄ表示される
+        
+#        if submit_btn:
+#            st.text(f'ようこそ!{name}さん!{address}より来ていただいてありがとう!')
+#            st.text(f'年齢層: {age_category}')
+#        hobby1=','.join(hobby)
+#        st.text(f'趣味: {hobby1}')
+with col2:    
+        image=Image.open(r'E:\250725div_app\data250725re\IMG250514_1356.jpg')
+        st.image(image,width=400)
+        
+        
+#        #data分析
+#        df=pd.read_csv('tokyo2024.csv',index_col='年月日')
+#        st.dataframe(df)
+#        #st.table(df)
+#        st.line_chart(df)
+#        st.bar_chart(df)
+        
+#        #matplotlib
+#        fig, ax=plt.subplots()
+#        ax.plot(df.index,df)
+#        ax.set_title('matplotlib graph')
+#        st.pyplot(fig)
+
+
+
+
